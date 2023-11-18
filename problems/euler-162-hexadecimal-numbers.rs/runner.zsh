24ee88d23dd287a47b5637d851b7154cde9e057f
Nino Candrlic <nino.candrlic2@mail.dcu.ie>
2023-11-18 13:25:49 +0000

    Created the rust solution

---
+++ problems/euler-162-hexadecimal-numbers.rs/runner.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+# Set environ variable
+TASK="euler-162-hexadecimal-numbers"
+
+# Exit on failure
+set -e
+
+# Compile rust code
+rustc -o ${TASK}_compiled ${TASK}.rs
+
+## test
+for v in 3 10 16
+do
+  ./show-exec-command ./${TASK}_compiled $v
+done
