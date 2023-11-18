fb8e60cc32f2ea5fe1883f486d4618d153918275
Nino Candrlic <nino.candrlic2@mail.dcu.ie>
2023-11-18 13:29:45 +0000

    Updated the rust solution

--- problems/euler-4-largest-palindrome-product.rs/euler-4-largest-palindrome-product.rs
+++ problems/euler-4-largest-palindrome-product.rs/euler-4-largest-palindrome-product.rs
@@ -1,54 +1,57 @@
+// <20% Problem 4
+
 use std::env;
 
-// <20% Problem 4
-// Function to check if a given number is a palindrome
-fn is_palindrome(n: u32) -> bool {
-    // Convert the number to a string for easier comparison
-    let s = n.to_string();
-    // Check if the characters in the string are equal when reversed
-    s.chars().eq(s.chars().rev())
+// Define a struct named `PalindromeChecker` to check if a number is a palindrome.
+struct PalindromeChecker {}
+
+impl PalindromeChecker {
+    // Define a function `is_palindrome` for the `PalindromeChecker` struct that checks if a given number `n` is a palindrome.
+    fn is_palindrome(n: u32) -> bool {
+        let s = n.to_string(); 
+        s.chars().eq(s.chars().rev()) 
+    }
 }
 
-// Function to find the largest palindrome product of two 3-digit numbers
-fn largest_palindrome_product() -> u32 {
-    let args: Vec<String> = env::args().collect();
-    let digits : u32 = match (&args[1]).parse::<u32>() {
-        Ok(i) => i,
-        Err(_) => {
-            eprintln!("Invalid Input");
-            0
-        }
-    };
+// Define a struct named `PalindromeFinder` to find the largest palindrome product of two numbers with the specified number of digits.
+struct PalindromeFinder {}
 
-    let base10 : u32 = 10;
-    let top = base10.pow(digits);
-    let bottom = base10.pow(digits-1);
+impl PalindromeFinder {
+    // Define a function `largest_palindrome_product` for the `PalindromeFinder` struct that finds the largest palindrome product of two numbers with the specified number of digits.
+    fn largest_palindrome_product(num_digits: u32) -> u32 {
+        let start_range = 10u32.pow(num_digits - 1);
+        let end_range = 10u32.pow(num_digits) - 1;
 
-    let mut largest_palindrome = 0;
-    // Iterate through all 3-digit numbers in descending order
-    for i in (bottom..top).rev() {
-        for j in (bottom..=i).rev() {
-            let product = i * j;
-            
-            // If the product is less than or equal to the current largest palindrome, break the inner loop
-            if product <= largest_palindrome {
-                break;
-            }
+        let mut largest_palindrome = 0; // Initialize the variable `largest_palindrome` as 0.
 
-            // Check if the product is a palindrome
-            if is_palindrome(product) {
-                largest_palindrome = product; // Update the largest palindrome if found
+        for i in (start_range..=end_range).rev() {
+            for j in (start_range..=i).rev() {
+                let product = i * j; // Calculate the product of `i` and `j`.
+
+                if product <= largest_palindrome {
+                    break; 
+                }
+
+                if PalindromeChecker::is_palindrome(product) {
+                    largest_palindrome = product; // Update `largest_palindrome` if the product is a palindrome.
+                }
             }
         }
+
+        largest_palindrome 
     }
-
-    largest_palindrome
 }
 
 fn main() {
-    // Find the largest palindrome product and store it in largest_palindrome
-    let largest_palindrome = largest_palindrome_product();
-    // Print the result to the console
-    println!("{}", largest_palindrome);
-}
+    // Get the number of digits from the command line argument.
+    let args: Vec<String> = env::args().collect();
 
+    if args.len() < 2 {
+        println!("Please provide the number of digits as a command line argument.");
+        return;
+    }
+
+    let num_digits: u32 = args[1].parse().expect("Invalid number of digits provided.");
+    let largest_palindrome = PalindromeFinder::largest_palindrome_product(num_digits); // Find the largest palindrome product.
+    println!( "{}", largest_palindrome);
+}
