727e90cb08cfe6ea43b88ae02f0c5232150ef6b0
Nino Candrlic <nino.candrlic2@mail.dcu.ie>
2023-11-18 13:04:08 +0000

    Updated the rust solution

--- problems/euler-10-summation-of-primes.rs/euler-10-summation-of-primes.rs
+++ problems/euler-10-summation-of-primes.rs/euler-10-summation-of-primes.rs
@@ -1,64 +1,74 @@
+// <20% Problem 10
+
 use std::env;
 
-// <20% Problem 10
-// Function to check if a number is prime.
-fn is_prime(n: u64) -> bool {
-  // Check if the number is exactly 2; 2 is prime.
-  if n == 2 {
-      return true;
-  }
-  
-  // Check if the number is less than 2; all numbers less than 2 are not prime.
-  if n < 2 {
-      return false;
-  }
-  
-  // Check if the number is even (except for 2); even numbers are not prime.
-  if n % 2 == 0 {
-      return false;
-  }
+struct PrimeCalculator {
+    total: u64,
+}
 
-  // Start checking for prime factors from 3 onwards.
-  let mut i = 3;
-  // Continue until the square of 'i' is less than or equal to 'n'.
-  while i * i <= n {
-      // If 'n' is divisible by 'i', it's not prime.
-      if n % i == 0 {
-          return false;
-      }
-      // Move to the next odd number to check for divisibility.
-      i += 2; // Only check odd numbers since even numbers have already been excluded.
-  }
+impl PrimeCalculator {
+    // Create a new instance of PrimeCalculator with the total initialized to 0
+    fn new() -> Self {
+        Self { total: 0 }
+    }
 
-  // If none of the conditions are met, the number is prime.
-  true
+    // Check if a number is prime
+    fn is_prime(&self, n: u64) -> bool {
+        if n == 2 {
+            return true;
+        }
+
+        if n < 2 {
+            return false;
+        }
+
+        if n % 2 == 0 {
+            return false;
+        }
+
+        let mut i = 3;
+        while i * i <= n {
+            if n % i == 0 {
+                return false;
+            }
+            i += 2;
+        }
+
+        true
+    }
+
+    // Calculate the sum of prime numbers up to a given range
+    fn calculate_sum(&mut self, range: u64) {
+        let mut i: u64 = 1;
+
+        while i < range {
+            if self.is_prime(i) {
+                self.total += i;
+            }
+            i += 1;
+        }
+    }
+
+    // Print the sum of prime numbers
+    fn print_sum(&self) {
+        println!("{}", self.total);
+    }
 }
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
-  let n : u64 = match (&args[1]).parse::<u64>() {
-    Ok(i) => i,
-    Err(_) => {
-        eprintln!("Invalid Input");
-        return
+    // Get the range from the command line arguments.
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Please provide the range as a command line argument.");
+        return;
     }
-  };
 
-
-
-  let mut i: u64 = 1; // Initialize a counter for numbers to be tested.
-  let mut total: u64 = 0; // Initialize a total to accumulate prime numbers.
-
-  // Iterate through numbers from 1 to 2,000,000.
-  while i < n {
-      // If 'i' is prime, add it to the 'total' accumulator.
-      if is_prime(i) {
-          total += i;
-      }
-      // Move on to the next number to be tested.
-      i = i + 1;
-  }
-
-  // Print the sum of prime numbers within the given range.
-  println!("{}", total);
+    let range: u64 = args[1].parse().expect("Invalid range provided.");
+    let mut prime_calculator = PrimeCalculator::new();
+    prime_calculator.calculate_sum(range); // Calculate the sum of prime numbers up to the given range
+    prime_calculator.print_sum();          
 }
+
+
+  
